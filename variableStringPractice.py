c08d879b6344d39be4c2a745e8a59d2772d7f2ce
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-07 20:45:12 -0500

    String and Variable practice first commit

---
+++ variableStringPractice.py
@@ -0,0 +1,18 @@
+#wallet variable with money. Half it.
+wallet = 335
+print(wallet/2)
+
+#simple strings
+name = "Eric"
+print(name)
+
+name = input("what is your name? ")
+age = input("How old are you? ")
+
+if age > str(40):
+    print(f"{name} you are a dino at {age}")
+else:
+    print(f"{name}, that is an awesome {age}")
+
+
+
