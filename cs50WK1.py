303e27ee1d5e9289d83f540a1368c39fd46dc656
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-08 22:44:20 -0500

    CS50WK1 deep.py

---
+++ cs50WK1.py
@@ -0,0 +1,17 @@
+# In deep.py, implement a program that prompts the user for the answer to
+# the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42
+# or (case-insensitively) forty-two or forty two. Otherwise output No.
+#deep.py
+
+gqol = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+answer = str(gqol)
+if answer == "42":
+    print("Yes")
+elif answer.lower() == "forty-two":
+    print("Yes")
+elif answer.lower() == "forty two":
+    print("Yes")
+else:
+    print("No")
+
+
