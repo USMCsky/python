191dbd758a949cf234455fd69a7a1f50c0efc4a9
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-08 22:51:40 -0500

    CS50WK1 deep.py

--- cs50WK1.py
+++ cs50WK1.py
@@ -5,7 +5,7 @@
 
 gqol = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 answer = str(gqol)
-if answer == "42":
+if answer.strip() == "42":
     print("Yes")
 elif answer.lower() == "forty-two":
     print("Yes")
