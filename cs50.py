cf4142d3eadf3fb1869394652465ab1f41472c7d
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-08 21:50:54 -0500

    Updated with einstein and tiip calculator

--- cs50.py
+++ cs50.py
@@ -26,9 +26,9 @@ e = (int(kg) * (300000000 * 300000000))
 print("E = " + str(e))
 
 # dollars_to_float, which should accept a str as input (formatted as $##.##
-# # wherein each # is a decimal digit), remove the leading $, and return the amount as a float. Given $50.00 as input, it should return 50.0.
+# # Each # is a decimal digit), remove the leading $, and return the amount as a float. Given $50.00 as input, it should return 50.0.
 # percent_to_float, which should accept a str as input (formatted as ##%
-# # wherein each # is a decimal digit), remove the trailing %, and return the percentage as a float. Given 15% as input, it should return 0.15.
+# # Each # is a decimal digit), remove the trailing %, and return the percentage as a float. Given 15% as input, it should return 0.15.
 # tip.py
 
 def main():
