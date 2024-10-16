97cb71751fcf52f0102f31084a0703fb82dbeb99
USMCsky <57950108+USMCsky@users.noreply.github.com>
2024-10-16 16:11:41 -0500

    Update CS50_WK3_SET.py

--- CS50WK3/CS50_WK3_SET.py
+++ CS50WK3/CS50_WK3_SET.py
@@ -1,5 +1,4 @@
 ### fuel.py
-
 while True :
     try:
         fraction = input("Fraction: ")
@@ -21,3 +20,21 @@ while True :
         pass
     else:
         break
+################################
+# grocery.py
+groceries = {}
+while True:
+    try:
+        item = input().upper()
+        if item in groceries.keys():
+            groceries[item] += 1
+        else:
+            groceries[item] = 1
+            pass
+    except EOFError:
+        sort = dict(sorted(groceries.items()))
+        break
+
+for item in sort:
+    print(groceries[item], item)
+  
