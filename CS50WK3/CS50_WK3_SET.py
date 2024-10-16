c5d95bd5e7e71cf2eb2865ec823d568c430d156a
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-15 23:57:32 -0500

    CS50 SET

--- CS50WK3/CS50_WK3_SET.py
+++ CS50WK3/CS50_WK3_SET.py
@@ -1,21 +1,21 @@
+### fuel.py
+
 while True :
     try:
         fraction = input("Fraction: ")
-        x,y = fraction.split("/")
+        x,y = fraction.split("/")   #split string into num/den
         newx = int(x)
         newy = int(y)
+        fuel = round(newx / newy * 100)
 
-        fuel = round(newx / newy *100)
-
-        if newx > newy:
+        if newx > newy:             #num > den start over
             continue
-
-        elif fuel >= 99:
-            print("F")
-        elif fuel <= 1 :
+        elif fuel <= 1:          # 1 or less empty
             print("E")
+        elif fuel >= 99:           # over 99 full
+            print("F")
         else:
-            print( str(fuel) + "%")
+            print( str(fuel) + "%")      # all good print
 
     except (ValueError, ZeroDivisionError):
         pass
