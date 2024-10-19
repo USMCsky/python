d121daa5a3279f1bc890f3b9e4ef4bf6a21c23b6
Eric Zabinsky <ezabinsky@gmail.com>
2024-10-19 11:25:59 -0500

    update

--- GeorgiaTech/advanced_loops.py
+++ GeorgiaTech/advanced_loops.py
@@ -49,5 +49,33 @@ for name1 in list1:
         if name1 == name2:
             print(name1)
 
+num = 5
+result = 1
+for i in range(1, num + 1):
+    result *= i
+print(result)
+
+###########################
+## All the following loops have the same result
+for i in range(1, 6):
+    j = 0
+    while j < i:
+        print(j, end=" ")
+        j += 1
+    print("")
+
+for i in range(1, 6):
+    for j in range(0, i):
+        print(j, end=" ")
+    print("")
+
+i = 0
+while i < 6:
+    j = 0
+    while j < i:
+        print(j, end=" ")
+        j += 1
+    i += 1
+    print("")
 
 
--- GeorgiaTech/test.py
+++ GeorgiaTech/test.py
@@ -2,4 +2,27 @@ num = 5
 result = 1
 for i in range(1, num + 1):
     result *= i
-    print(result)
+print(result)
+###########################
+## All the following loops have the same result
+for i in range(1, 6):
+    j = 0
+    while j < i:
+        print(j, end = " ")
+        j += 1
+    print("")
+
+for i in range(1, 6):
+    for j in range(0, i):
+        print(j, end = " ")
+    print("")
+
+i = 0
+while i < 6:
+    j = 0
+    while j < i:
+        print(j, end = " ")
+        j += 1
+    i += 1
+    print("")
+
